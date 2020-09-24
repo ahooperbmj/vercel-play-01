678bc2e5ea192c17db66b3921cd11ccf002fc7ad
Alex Hooper <ahooper@bmj.com>
2020-09-24 07:01:05 +0100

    a new page

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+//import '../styles/index.css';
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <h1>About me</h1>
+      <p>I know, i know</p>
+    </div>
+  )
+}
+
