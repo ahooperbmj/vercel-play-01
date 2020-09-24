943348ec6a95aace098bb1c73f366699c63009e8
Alex Hooper <ahooper@bmj.com>
2020-09-24 14:06:04 +0100

    trying an iframe...

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,8 @@ export default function About() {
   return (
     <div style={{ color: `teal` }}>
       <h1>About me</h1>
-      <p>I know, i know</p>
+      <p>BMJ Chicken trans time</p>
+      <iframe src="https://frame-rpm.eu.newrelic.com/public/charts/2uWQc4lRabS" width="500" height="300" scrolling="no" frameborder="no"></iframe>
     </div>
   )
 }
