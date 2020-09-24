01be82262dd6017f012c5372057419bac3bc2f11
Alex Hooper <ahooper@bmj.com>
2020-09-24 14:15:22 +0100

    change front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,32 +15,17 @@ function Index() {
   return (
     <main>
       <Helmet>
-        <title>Gatsby + Node.js (TypeScript) API</title>
+        <title>Experiments with Gatsby... </title>
       </Helmet>
-      <h1>Gatsby + Node.js (TypeScript) API</h1>
+      <h1>Bits and pieces</h1>
       <h2>
-        Deployed with{' '}
         <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
+          href="/about"
         >
-          Vercel
-        </a>
-        !
+          BMJ Chicken graphs
+        </a>{' '}
       </h2>
       <p>
-        <a
-          href="https://github.com/vercel/vercel/blob/master/gatsby"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        is a <a href="https://www.gatsbyjs.org/">Gatsby</a> app with two
-        directories, <code>/src</code> for static content and <code>/api</code>{' '}
-        which contains a serverless{' '}
-        <a href="https://nodejs.org/en/">Node.js (TypeScript)</a> function. See{' '}
         <a href="/api/date">
           <code>api/date</code> for the Date API with Node.js (TypeScript)
         </a>
