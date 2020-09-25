1b3d0c9fe3358a5a944080b358cdde96841bbbf1
Alex Hooper <ahooper@bmj.com>
2020-09-25 06:33:10 +0100

    fix?

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ export default function About() {
       <p>BMJ Chicken trans time</p>
       <iframe src="https://frame-rpm.eu.newrelic.com/public/charts/2uWQc4lRabS" width="500" height="300" scrolling="no" frameborder="no"></iframe>
       <p>
-      <img src="https://gorgon.service.eu.newrelic.com/image/1109ed8a-dee4-4e3f-a0c8-333855e80bef?type=area">
+      <img src="https://gorgon.service.eu.newrelic.com/image/1109ed8a-dee4-4e3f-a0c8-333855e80bef?type=area"></img>
       </p>
     </div>
   )
