4f7f6a7f78351d3d78c5682c8ae7d7414f369d38
Alex Hooper <ahooper@bmj.com>
2020-09-25 06:26:35 +0100

    another image

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,9 @@ export default function About() {
       <h1>About me</h1>
       <p>BMJ Chicken trans time</p>
       <iframe src="https://frame-rpm.eu.newrelic.com/public/charts/2uWQc4lRabS" width="500" height="300" scrolling="no" frameborder="no"></iframe>
+      <p>
+      <img src="https://gorgon.service.eu.newrelic.com/image/1109ed8a-dee4-4e3f-a0c8-333855e80bef?type=area">
+      </p>
     </div>
   )
 }
