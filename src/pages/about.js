9182d22c4c31f4492fd794524531e62773847c4b
Alex Hooper <ahooper@bmj.com>
2020-09-25 06:46:30 +0100

    further dashboarding

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,16 @@ export default function About() {
       <p>BMJ Chicken trans time</p>
       <iframe src="https://frame-rpm.eu.newrelic.com/public/charts/2uWQc4lRabS" width="500" height="300" scrolling="no" frameborder="no"></iframe>
       <p>
-      <img src="https://gorgon.service.eu.newrelic.com/image/1109ed8a-dee4-4e3f-a0c8-333855e80bef?type=area"></img>
+      Apdex score:
+      <img src="https://chart-embed.service.eu.newrelic.com/herald/d8a6323c-2997-408a-b547-b955ef0a866e?height=400px&timepicker=true" alt="Apdex"></img>
+      </p>
+      <p>
+      Throughput:
+      <img src="https://chart-embed.service.eu.newrelic.com/herald/10a5a8ac-4f52-47b4-9a9d-8dec45f9f47c?height=400px&timepicker=true" alt="Throughput"></img>
+      </p>
+      <p>
+      Top 5 Web Trans:
+      <img src="https://chart-embed.service.eu.newrelic.com/herald/3515f93a-af11-4fdc-bbb7-1235f4155706?height=400px&timepicker=true" alt="Top 5 Web Trans"></img>
       </p>
     </div>
   )
